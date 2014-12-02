c3dac591b79be137b49e2e7fb70b065c2bf71c33
Matt Spraggs <matthew.spraggs@gmail.com>
2014-12-02 23:17:38 +0000

    Added more tests to test_lattice

--- pyQCD2/test/test_lattice.py
+++ pyQCD2/test/test_lattice.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 
 from mpi4py import MPI
+import numpy as np
 import pytest
 
 from pyQCD2.core.lattice import Lattice
@@ -21,6 +22,8 @@ def lattice_params():
     params['mpishape'] = tuple(MPI.Compute_dims(nprocs, len(latshape)))
     params['locshape'] = tuple([x // y for x, y in zip(params['latshape'],
                                                        params['mpishape'])])
+    params['datashape'] = tuple([x // y + 2 for x, y in zip(params['latshape'],
+                                                            params['mpishape'])])
     params['locvol'] = reduce(lambda x, y: x * y,
                               params['locshape'])
     params['ndims'] = len(latshape)
@@ -33,6 +36,7 @@ class TestLattice(object):
 
     def test_init(self, lattice_params):
         """Test the constructor"""
+        # TODO: Test in here for neighbour ranks
         lattice = Lattice((8, 4, 4, 4), 1)
         for key, value in lattice_params.items():
             assert getattr(lattice, key) == value
@@ -43,3 +47,28 @@ class TestLattice(object):
             with pytest.raises(RuntimeError):
                 nprocs = lattice_params['nprocs']
                 Lattice((nprocs + 1, nprocs, nprocs, nprocs))
+
+    def test_ishere(self):
+        """Test Lattice.ishere"""
+        lattice = Lattice((8, 4, 4, 4), 1)
+        result = lattice.ishere((0, 0, 0, 0))
+        if lattice.comm.Get_rank() == 0:
+            assert result
+        else:
+            assert not result
+
+    def test_get_local_coords(self):
+        """Test Lattice.get_local_coords"""
+        lattice = Lattice((8, 4, 4, 4), 1)
+        assert lattice.get_local_coords((0, 0, 0, 0)) == (1, 1, 1, 1)
+        assert lattice.get_local_coords((7, 3, 3, 3)) == lattice.locshape
+
+    def test_get_local_index(self):
+        """Test Lattice.get_local_index"""
+        lattice = Lattice((8, 4, 4, 4), 1)
+        first_index = reduce(lambda x, y: x * y[0] + y[1],
+                             zip(lattice.datashape[-1:0:-1],
+                                 (1, 1, 1)), 1)
+        assert lattice.get_local_index((0, 0, 0, 0)) == first_index
+        assert (lattice.get_local_index((7, 3, 3, 3))
+                == np.prod(lattice.data_shape) - first_index - 1)
